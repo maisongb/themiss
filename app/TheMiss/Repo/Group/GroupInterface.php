d08385fdbfd8f8fde833fdd64c28cc4443b73cb7
Ionut Tanasa <ionutz2k@gmail.com>
2014-03-13 22:31:27 +0100

    Created Group Management

---
+++ app/TheMiss/Repo/Group/GroupInterface.php
@@ -0,0 +1,52 @@
+<?php namespace TheMiss\Repo\Group;
+
+interface GroupInterface
+{
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @ret¶urn Response
+     */
+    public function store($data);
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  int $id
+     * @return Response
+     */
+    public function update($id);
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int $id
+     * @return Response
+     */
+    public function destroy($id);
+
+    /**
+     * Return a specific user by a given id
+     *
+     * @param  integer $id
+     * @return User
+     */
+    public function byId($id);
+
+    /**
+     * Return a specific user by a given name
+     *
+     * @param  string $name
+     * @return User
+     */
+    public function byName($name);
+
+    /**
+     * Return all the registered users
+     *
+     * @return stdObject Collection of users
+     */
+    public function all();
+
+}
